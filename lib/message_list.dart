c5fa5bc16c948e3070a594adcd0c5afe76c9ec43
SontranBK <97107855+SontranBK@users.noreply.github.com>
2022-04-15 22:13:28 +0700

    add new FCM mess feature

--- lib/message_list.dart
+++ lib/message_list.dart
@@ -22,47 +22,71 @@ class _MessageList extends State<MessageList> {
       String? titleOfNoti = Noti.title ?? '';
       String? bodyOfNoti = Noti.body ?? '';
 
-      List Info = [];
 
-      int idx = bodyOfNoti.indexOf("|");
-      //print('idx: $idx');
-      Info.add(bodyOfNoti.substring(0,idx).trim());
-      //print('Info: $Info');
-      String Rest = bodyOfNoti.substring(idx+1);
-      //print('Rest of string: $Rest');
-      //print('Info: $Info');
-
-      while (idx != 22)
-      {
-        idx = Rest.indexOf("|");
-        //print('idx: $idx');
-        Info.add(Rest.substring(1,idx).trim());
-        //print('Info: $Info');
-        Rest = Rest.substring(idx+1);
-        //print('Rest of string: $Rest');
+      print(bodyOfNoti);
+      if (titleOfNoti == 'Start') {
+        showDialog(
+            context: context,
+            builder: (context) {
+              Future.delayed(Duration(seconds: 5), () {
+                Navigator.of(context).pop(true);
+              });
+              return AlertDialog(
+                title: Text('Bắt đầu đọc thẻ',
+                  style: TextStyle(color: Colors.blue,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                content: Text('Quá trình khởi động hoàn tất\nBắt đầu đọc thẻ NFC'),
+              );
+            }
+        );
       }
-      Info.add(Rest);
-      //print('Info: $Info');
 
-      showDialog(
-         context: context,
-         builder: (context) {
-           Future.delayed(Duration(seconds: 3), () {
-             Navigator.of(context).pop(true);
-           });
-           return AlertDialog(
-             title: Text('Thông tin quẹt thẻ',
-               style: TextStyle(color: Colors.blue, fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-             content: Text('- Tên học sinh:  '+Info[0]+'\n'
-			   '- Id học sinh:  '+Info[1]+'\n'
-                           '- Lớp:  '+Info[2]+'\n'
-                 '- Trường:  '+Info[3]+'\n'
-                 '- Ngày giờ quẹt thẻ:  '+Info[4]+'\n'
-                 '- Mã thẻ:  '+Info[5]),
-           );
-         }
-       );
+      if (titleOfNoti == "NFC_card_info") {
+        List Info = [];
+
+        int idx = bodyOfNoti.indexOf("|");
+        print('idx: $idx');
+        Info.add(bodyOfNoti.substring(0, idx).trim());
+        print('Info: $Info');
+        String Rest = bodyOfNoti.substring(idx + 1);
+        print('Rest of string: $Rest');
+        print('Info: $Info');
+
+        while (idx != 22) {
+          idx = Rest.indexOf("|");
+          print('idx: $idx');
+          Info.add(Rest.substring(1, idx).trim());
+          print('Info: $Info');
+          Rest = Rest.substring(idx + 1);
+          print('Rest of string: $Rest');
+        }
+        Info.add(Rest);
+        print('Info: $Info');
+
+        showDialog(
+            context: context,
+            builder: (context) {
+              Future.delayed(Duration(seconds: 3), () {
+                Navigator.of(context).pop(true);
+              });
+              return AlertDialog(
+                title: Text('Thông tin quẹt thẻ',
+                  style: TextStyle(color: Colors.blue,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                content: Text('- Tên học sinh:  ' + Info[0] + '\n'
+                    '- Id học sinh:  ' + Info[1] + '\n'
+                    '- Lớp:  ' + Info[2] + '\n'
+                    '- Trường:  ' + Info[3] + '\n'
+                    '- Ngày giờ quẹt thẻ:  ' + Info[4] + '\n'
+                    '- Mã thẻ:  ' + Info[5]),
+              );
+            }
+        );
+      }
     });
   }
 
