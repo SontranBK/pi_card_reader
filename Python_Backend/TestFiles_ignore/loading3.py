85bfaa4c7b434f980702bd5bc9a5f30480fcbb9b
SontranBK <97107855+SontranBK@users.noreply.github.com>
2022-05-09 16:23:09 +0700

    update loading file

--- Python_Backend/TestFiles_ignore/loading3.py
+++ Python_Backend/TestFiles_ignore/loading3.py
@@ -7,10 +7,7 @@ total_time = 300
 i = 0
 def animate():
     i =0
-    for c in itertools.cycle(["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]):
-        #if done:
-          #break
-        #sys.stdout.write('\rloading' + c)
+    for c in itertools.cycle(["[■ □ □ □ □ □ □ □ □ □]","[■ ■ □ □ □ □ □ □ □ □]", "[■ ■ ■ □ □ □ □ □ □ □]", "[■ ■ ■ ■ □ □ □ □ □ □]", "[■ ■ ■ ■ ■ □ □ □ □ □]", "[■ ■ ■ ■ ■ ■ □ □ □ □]", "[■ ■ ■ ■ ■ ■ ■ □ □ □]", "[■ ■ ■ ■ ■ ■ ■ ■ □ □]", "[■ ■ ■ ■ ■ ■ ■ ■ ■ □]", "[■ ■ ■ ■ ■ ■ ■ ■ ■ ■]"]):
         sys.stdout.write('\rloading' + c + ' process ' + str(i) + '/' + str(total_time) + ' ' + '{:.2f}'.format(i / total_time * 100) + '%')
         sys.stdout.flush()
         time.sleep(0.5)
