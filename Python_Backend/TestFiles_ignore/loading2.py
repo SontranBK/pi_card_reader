8c9299c4396c945edc1d5e07825dcf6a9cff5cc6
SontranBK <97107855+SontranBK@users.noreply.github.com>
2022-05-09 19:29:04 +0700

    .

--- Python_Backend/TestFiles_ignore/loading2.py
+++ Python_Backend/TestFiles_ignore/loading2.py
@@ -37,7 +37,7 @@ class Loading:
                 Label(self.root, bg="#ECECEC", width=2, height=1).place(x=(j + 22)*22,y=350)
             end = time.time()
             
-            print(f"Time left: {self.time_left}")
+            #print(f"Time left: {self.time_left}")
             self.time_left = self.time_left - (end-start)
             Rate =round((self.time_to_run - self.time_left)*100/self.time_to_run,2)
             Label(self.root,text='Tiến độ khởi tạo: '+str(Rate)+'%', font = "Bahnschrift 15", bg= "black", fg ="#FFBD09").place(x=490, y=380)
