429c3a080a85a76fb47a2cd2503d8a01b6c9cae0
SontranBK <97107855+SontranBK@users.noreply.github.com>
2022-05-09 15:29:45 +0700

    Add files via upload

---
+++ Python_Backend/TestFiles_ignore/database.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# 1A1
+# 1083682100084
+
+import sqlite3
+import time
+from datetime import datetime,date
+
+conn = sqlite3.connect('/home/thien-nv/pi_card_reader/Database/SampleDB.db')
+
+print ("Opened database successfully\n")
+
+while(True):
+	inputClass = input("Input Class: ")
+	inputID = input("Input ID: ")
+	timeSentToServer = date.today().strftime('%Y-%m-%d')
+	print(f"time sent to server: {timeSentToServer}")
+	start = time.time()
+
+	cursor = conn.execute(f"SELECT name, id, dateofbirth, time_a, error_code_a, time_b, error_code_b from CLASS_{inputClass} where ID = {inputID}")
+	for row in cursor:
+		print (f"\nFind student with following info:\nNAME = {row[0]}\nID = {row[1]}\nDoB = {row[2]}\nTime A = {row[3]}\nTime B = {row[5]}\n")
+		if (row[3] == None):
+			#row[3] = timeSentToServer
+			conn.execute(f"UPDATE CLASS_{inputClass} set TIME_A = {timeSentToServer} where ID = {inputID}")
+			conn.commit()
+			print (f"After updating time:\nNAME = {row[0]}\nID = {row[1]}\nDoB = {row[2]}\nTime A = {row[3]}\nTime B = {row[5]}\n")
+		else:
+			#row[5] = timeSentToServer
+			conn.execute(f"UPDATE CLASS_{inputClass} set TIME_B = {timeSentToServer} where ID = {inputID}")
+			conn.commit()
+			print (f"After updating time:\nNAME = {row[0]}\nID = {row[1]}\nDoB = {row[2]}\nTime A = {row[3]}\nTime B = {row[5]}\n")
+	end = time.time()
+	print("Execution time: "+str(end-start))
+	
+
+conn.close()
