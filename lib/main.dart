1f88bd16a6d09c30234fa34c866986bccfd4b5a0
SontranBK <97107855+SontranBK@users.noreply.github.com>
2022-05-11 14:08:33 +0700

    Update UI to the newest

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/painting.dart';
 import 'package:intl/intl.dart';
 
 import 'dart:html' as webFile;
 import 'dart:async';
-//import 'dart:convert';
 import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-//import 'package:http/http.dart' as http;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'message_list.dart';
 //import 'message.dart';
 import 'firebase_options.dart';
+import 'package:flutter/rendering.dart';
+
+
+void highlightRepaints() {
+  debugRepaintRainbowEnabled = true;
+}
+
 
 
 /// Define a top-level named handler which background/terminated messages will
@@ -117,27 +124,65 @@ class MyApp extends StatelessWidget {
               image: AssetImage("assets/wallpaper.jpg"),
             ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-            Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:<Column>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 25, top:10),
+                      child: Row(
+                        children: [
+                          Image.asset('THCS_CG.png', scale: 6,),
+                          const Text('Trường THCS Cầu Giấy',
+                            style: TextStyle(
+                              fontFamily: 'Dosis', fontSize: 32, fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                        ],
+                      )
+                  )
+                ],
+              ),
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Expanded(
                       child: Container(
-                        padding: const EdgeInsets.only(right:50.0,top:25.0),
+                        padding: const EdgeInsets.only(right: 50,top:28.0),
                         //color: Colors.white,
                         child: ShowDateTime(),
                       ),
                     ),
-            // This expands the row element vertically because it's inside a column
-            Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.only(right:100.0,top:25.0,left:20.0),
-                    //color: Colors.white,
-                    child: MessageList(),
-                  ),
-                ),
-            ]
-          ),
+                    // This expands the row element vertically because it's inside a column
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.only(right:100,top:25.0,left:20.0),
+                        //color: Colors.white,
+                        child: MessageList(),
+                      ),
+                    ),
+                    Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(right: 150, top: 25, left: 20),
+                          child: Text(
+                            'Tri thức là chìa khóa mở cửa tương lai.',
+                            style: TextStyle(
+                              fontFamily: 'Dosis' , fontSize: 32, fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                    )
+                  ]
+              ),
+
+            ],
+          ),//Row
         ),
         ),
       );
@@ -153,6 +198,8 @@ class ShowDateTime extends StatefulWidget {
 
 class _ShowDateTimeState extends State<ShowDateTime> {
   String _timeString ='';
+  String _gio = ' ', _thu = ' ', _ngay = ' ', _thang =  ' ';
+  var weekday = {'Mon':'Thứ Hai', 'Tue':'Thứ Ba', 'Wed':'Thứ Tư', 'Thu':'Thứ Năm', 'Fri':'Thứ Sáu', 'Sat':'Thứ Bảy', 'Sun':'Chủ Nhật'};
 
   @override
   void initState() {
@@ -166,8 +213,9 @@ class _ShowDateTimeState extends State<ShowDateTime> {
    return Text(
      _timeString,
      style: TextStyle(
+       fontFamily: 'Dosis', fontWeight: FontWeight.w400,
        color: Colors.white,
-       fontSize: 35.0,
+       fontSize: 32.0,
      ),
    );
   }
@@ -183,8 +231,17 @@ class _ShowDateTimeState extends State<ShowDateTime> {
   }
 
   String _formatDateTime(DateTime dateTime) {
-    return DateFormat('hh:mm - EE,dd/MM').format(dateTime);
+    String _time_dis = ' ';
+    _gio = DateFormat('hh:mm').format(dateTime);
+    _thu = weekday[DateFormat('EEE').format(dateTime)].toString();
+    _time_dis = _gio +' - '+ _thu+', '+DateFormat('dd').format(dateTime) + ' thg ' +DateFormat('MM').format(dateTime);
+    return _time_dis;
+    // return DateFormat('hh:mm - EE, dd MM').format(dateTime);
+    // return DateFormat('hh:mm - EE,dd/MM' {locale: VN}).format(dateTime);
     //return DateFormat('MM/dd/yyyy hh:mm:ss').format(dateTime);
   }
+
 }
+
+
 
--- lib/message_list.dart
+++ lib/message_list.dart
@@ -17,6 +17,15 @@ class _MessageList extends State<MessageList> {
   @override
   void initState() {
     super.initState();
+    var _ID = " ";
+    var _class = " ";
+    var _textSt = TextStyle(
+    decoration: TextDecoration.none,
+    fontSize: 30,
+    color: Colors.white70,
+    fontFamily: 'Dosis', fontWeight: FontWeight.w400,
+    );
+
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       Noti = message.notification ?? RemoteNotification();
       String? titleOfNoti = Noti.title ?? '';
@@ -104,27 +113,108 @@ class _MessageList extends State<MessageList> {
         Info.add(Rest);
         print('Info: $Info');
 
-        showDialog(
-            context: context,
-            builder: (context) {
-              Future.delayed(Duration(seconds: 3), () {
-                Navigator.of(context).pop(true);
-              });
-              return AlertDialog(
-                title: Text('Thông tin quẹt thẻ',
-                  style: TextStyle(color: Colors.blue,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold),
+        showGeneralDialog(
+          context: context,
+          barrierLabel: "Barrier",
+          barrierDismissible: true,
+          barrierColor: Colors.black.withOpacity(0.1),
+          transitionDuration: Duration(milliseconds: 500),
+          pageBuilder: (_, __, ___) {
+            Future.delayed(Duration(seconds: 5), () {
+              Navigator.of(context).pop(true);
+            });
+            return Center(
+              child: Container(
+                padding: EdgeInsets.only (top:0),
+                height: 180,
+                width: 550,
+                margin: EdgeInsets.only(top: 50, left: 750),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      stops: [
+                        0.0,
+                        0.33,
+                        0.68,
+                        0.99,
+                      ],
+                      colors: [
+                        Colors.lightBlue.withOpacity(0.6),
+                        Colors.lightBlue.withOpacity(0.0),
+                        Colors.lightBlue.withOpacity(0.0),
+                        Colors.lightBlue.withOpacity(0.6),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(40),
                 ),
-                content: Text('- Tên học sinh:  ' + Info[0] + '\n'
-                    '- Id học sinh:  ' + Info[1] + '\n'
-                    '- Lớp:  ' + Info[2] + '\n'
-                    '- Trường:  ' + Info[3] + '\n'
-                    '- Ngày giờ quẹt thẻ:  ' + Info[4] + '\n'
-                    '- Mã thẻ:  ' + Info[5]),
-              );
+                child: SizedBox.expand(
+                  child: Row(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 30, right: 30),
+                            width: 100, height: 100,
+                            decoration: BoxDecoration(
+                              color: Colors.white70,
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 2,
+                              )
+                            ),
+                            child: Image.asset('THCS_CG.png', scale: 6,),
+                          )
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            Info[0],
+                            style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 28,
+                              color: Colors.white,
+                              fontFamily: 'Dosis', fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            'ID: '+Info[1],
+                            style: _textSt,
+                          ),
+                          Text(
+                            'Lớp: '+Info[2],
+                            style: _textSt,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+          transitionBuilder: (_, anim, __, child) {
+            Tween<Offset> tween;
+            if (anim.status == AnimationStatus.reverse) {
+              tween = Tween(begin: Offset(-1, 0), end: Offset.zero);
+            } else {
+              tween = Tween(begin: Offset(1, 0), end: Offset.zero);
             }
-        );
+
+            return SlideTransition(
+              position: tween.animate(anim),
+              child: FadeTransition(
+                opacity: anim,
+                child: child,
+              ),
+            );
+          },
+        );//show dialog
       }
     });
   }
@@ -135,3 +225,4 @@ class _MessageList extends State<MessageList> {
   }
 
 }
+
