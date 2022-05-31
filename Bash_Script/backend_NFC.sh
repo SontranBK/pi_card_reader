1a51598129be64c1e9e873dc4c96f8d99025070a
SontranBK <97107855+SontranBK@users.noreply.github.com>
2022-05-31 22:48:43 +0700

    improve timing

--- Bash_Script/backend_NFC.sh
+++ Bash_Script/backend_NFC.sh
@@ -1,5 +1,14 @@
 cd ~
 echo 1 | sudo -S cp pi_card_reader/Python_Backend/main_NFC_Mifare_DU950.py ~
 echo 1 | sudo -S cp pi_card_reader/Python_Backend/service-account.json ~
-sleep 30
+sleep 45
+echo 1 | sudo -S rm API_TOKEN\ \(1\).txt
+echo 1 | sudo -S rm API_TOKEN\ \(2\).txt
+echo 1 | sudo -S rm API_TOKEN\ \(3\).txt
+echo 1 | sudo -S rm API_TOKEN\ \(4\).txt
+echo 1 | sudo -S rm API_TOKEN\ \(5\).txt
+echo 1 | sudo -S rm API_TOKEN\ \(6\).txt
+echo 1 | sudo -S rm API_TOKEN\ \(7\).txt
+echo 1 | sudo -S rm API_TOKEN\ \(8\).txt
+echo 1 | sudo -S rm API_TOKEN\ \(9\).txt
 echo 1 | sudo -S python3 main_NFC_Mifare_DU950.py
