bc041fbbb989d298f8f3a862247291f0e0ee2f2e
SontranBK <97107855+SontranBK@users.noreply.github.com>
2022-06-12 21:49:17 +0700

    update logo tools and docs

---
+++ Tools_And_Docs/Logo_School_Tool/update_logo_school.sh
@@ -0,0 +1,8 @@
+cd ~
+cd pi_card_reader/
+sudo rm -r build/
+cd ~
+cd pi_card_reader/
+sudo flutter build web
+cd ~
+sudo cp pi_card_reader/build/web/assets/assets/logo.png pi_card_reader/build/web/assets/
