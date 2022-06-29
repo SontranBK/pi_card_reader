fadbcf53389c7b7c1087d230f4c42253d9e04d87
SontranBK <97107855+SontranBK@users.noreply.github.com>
2022-06-29 11:01:22 +0700

    update file

--- Tools_And_Docs/Logo_School_Tool/update_logo_school.sh
+++ Tools_And_Docs/Logo_School_Tool/update_logo_school.sh
@@ -3,14 +3,8 @@ cd pi_card_reader/
 echo 1 | sudo -S rm -r build/
 cd ~
 cd pi_card_reader/
-echo 1 | sudo -S flutter build web
+echo 1 | sudo -S flutter build web --release --web-renderer html
 cd ~
-echo 1 | sudo -S cp pi_card_reader/build/web/assets/assets/logo.png pi_card_reader/build/web/assets/
-
-echo 1 | sudo -S cp pi_card_reader/assets/logo.png pi_card_reader/build/web/assets/
-echo 1 | sudo -S cp pi_card_reader/assets/logo.png pi_card_reader/build/web/assets/assets/
-echo 1 | sudo -S cp pi_card_reader/assets/background.jpg pi_card_reader/build/web/assets/
-echo 1 | sudo -S cp pi_card_reader/assets/background.jpg pi_card_reader/build/web/assets/assets/
 echo 1 | sudo -S rm pi_card_reader/build/web/assets/left.png
 echo 1 | sudo -S rm pi_card_reader/build/web/assets/assets/left.png
 echo 1 | sudo -S rm pi_card_reader/build/web/assets/right.png
